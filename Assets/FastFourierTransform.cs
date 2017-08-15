240701503cbea0815c64424f7d5a03144ba38cee
Thiago Figueira <tsantosfigueira@outlook.com>
2017-08-14 21:21:13 -0400

    FFT Implementation

---
+++ Assets/FastFourierTransform.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using System.Numerics;
+
+public class FastFourierTransform : MonoBehaviour
+{
+    /* Performs a Bit Reversal Algorithm on a postive integer 
+     * for given number of bits
+     * e.g. 011 with 3 bits is reversed to 110 */
+    public static int BitReverse(int n, int bits)
+    {
+        int reversedN = n;
+        int count = bits - 1;
+
+        n >>= 1;
+        while (n > 0)
+        {
+            reversedN = (reversedN << 1) | (n & 1);
+            count--;
+            n >>= 1;
+        }
+
+        return ((reversedN << count) & ((1 << bits) - 1));
+    }
+
+    /* Uses Cooley-Tukey iterative in-place algorithm with radix-2 DIT case
+     * assumes no of points provided are a power of 2 */
+    public static void FFT(Complex[] buffer)
+    {
+
+        int bits = (int)Math.Log(buffer.Length, 2);
+        for (int j = 1; j < buffer.Length / 2; j++)
+        {
+
+            int swapPos = BitReverse(j, bits);
+            var temp = buffer[j];
+            buffer[j] = buffer[swapPos];
+            buffer[swapPos] = temp;
+        }
+
+        for (int N = 2; N <= buffer.Length; N <<= 1)
+        {
+            for (int i = 0; i < buffer.Length; i += N)
+            {
+                for (int k = 0; k < N / 2; k++)
+                {
+
+                    int evenIndex = i + k;
+                    int oddIndex = i + k + (N / 2);
+                    var even = buffer[evenIndex];
+                    var odd = buffer[oddIndex];
+
+                    double term = -2 * Math.PI * k / (double)N;
+                    Complex exp = new Complex(Math.Cos(term), Math.Sin(term)) * odd;
+
+                    buffer[evenIndex] = even + exp;
+                    buffer[oddIndex] = even - exp;
+                }
+            }
+        }
+    }
+}
